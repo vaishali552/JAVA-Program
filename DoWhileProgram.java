8e9acb399de12004df92b89a8c50253231f20561
vaishali R <vaishaliravichandran2003@gmail.com>
2024-12-23 13:53:20 +0530

    Add files via upload

---
+++ DoWhileProgram.java
@@ -0,0 +1,21 @@
+package JavaPackage;
+
+public class DoWhileProgram {
+	public static void main(String []args) {
+// TODO Auto-generated method stub
+// do
+//(Statement execute)
+//while(condition
+		int i=25;
+
+		do {
+			System.out.println("Hello world");
+			System.out.println(i);
+			i--;
+		}
+		while(1>0);
+
+
+		
+	}
+}
